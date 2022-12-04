79aecfc6ee9d74a45f8499a3f18d7e4d45372b9f
MantisOlthoi <MantisOlthoi@gmail.com>
2022-12-04 00:57:44 -0800

    Accidentally checked in binaries. Oops

--- Day1/target/release/Day1.d
+++
@@ -1,1 +0,0 @@
-E:\Code\Advent\2022\Day1\target\release\Day1.exe: E:\Code\Advent\2022\Day1\src\main.rs
--- Day1/target/release/deps/Day1.d
+++
@@ -1,5 +0,0 @@
-E:\Code\Advent\2022\Day1\target\release\deps\Day1.exe: src\main.rs
-
-E:\Code\Advent\2022\Day1\target\release\deps\Day1.d: src\main.rs
-
-src\main.rs:
